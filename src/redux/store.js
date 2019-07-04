7764c463974b3a53e6dfd8f48779d6c5af3da4db
bpalij <bpalij@gmail.com>
2019-07-04 19:36:47 +0300

    Store prototype
    First prototype of store and reducers

---
+++ src/redux/store.js
@@ -0,0 +1,64 @@
+import { combineReducers, createStore } from 'redux';
+import { START_LOAD_DATA, LOADED_DATA } from './actionTypes';
+
+const disableInput = (state = true, action) => {
+  switch (action.type){
+    case START_LOAD_DATA:
+      return true;
+    case LOADED_DATA:
+      return false;
+    default:
+      return state;
+  }
+}
+
+const data = (state = {}, action) => {
+  switch (action.type) {
+    case LOADED_DATA:
+      return action.data.data;
+    default:
+      return state;
+  }
+}
+
+const headers = (state = {}, action) => {
+  switch (action.type) {
+    case LOADED_DATA:
+      return action.data.headers 
+    default:
+      return state;
+  }  
+}
+
+const defaultPaginator = {
+  page: 1,
+  pages: 0,
+  disableFirst: true,
+  disableLast: true,
+}
+
+const paginator = (state = defaultPaginator, action) => {
+  switch (action.type) {
+    case LOADED_DATA:
+      const headerPage = action.data.headers['x-pagination-page'];
+      const headerPages = action.data.headers['x-pagination-page-count'];
+      return {
+        page: headerPage || 1,
+        pages: headerPages || 0,
+        disableFirst: !!(typeof(headerPage)!=='number' || headerPage<=1),
+        disableLast: !!(typeof(headerPages)!=='number' || headerPage>=headerPages),
+      };
+    default:
+      return state;
+  }
+}
+
+// TODO add reducer for filters
+
+const store = createStore(combineReducers({
+  disableInput,
+  data,
+  headers,
+  paginator,
+}));
+export default store;
