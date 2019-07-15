3ae0e034afd117356329ef89c1d192ae5625c891
bpalij <bpalij@gmail.com>
2019-07-15 17:39:02 +0300

    Adapting Table to airbnb-eslint

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,14 +5,13 @@ function getNumber(headers, index) {
   const page = +headers['x-pagination-page'];
   const limit = +headers['x-pagination-limit'];
   const pages = +headers['x-pagination-page-count'];
-  if(!page || !limit || !pages){
+  if (!page || !limit || !pages) {
     return '';
-  } else {
-    return ((page-1)*limit+(index+1));
   }
+  return ((page - 1) * limit + (index + 1));
 }
 
-function Table(props){
+function Table(props) {
   const { headers, data, images } = props;
   return (
     <table className="form-table-width main-table">
@@ -26,26 +25,24 @@ function Table(props){
         </tr>
       </thead>
       <tbody>
-        {data.map((x, i) => {
-          return (
-            <tr key={x.show ? x.show.ids.trakt : x.ids.trakt}>
-              <td>{getNumber(headers, i)}</td>
-              <td><img src={images[i]} alt={`${x.show ? x.show.title : x.title} poster`} /></td>
-              <td>{x.show ? x.show.title : x.title}</td>
-              <td>{x.watchers}</td>
-              <td>{x.show ? x.show.year : x.year}</td>
-            </tr>
-          )
-        })}
+        {data.map((x, i) => (
+          <tr key={x.show ? x.show.ids.trakt : x.ids.trakt}>
+            <td>{getNumber(headers, i)}</td>
+            <td><img src={images[i]} alt={`${x.show ? x.show.title : x.title} poster`} /></td>
+            <td>{x.show ? x.show.title : x.title}</td>
+            <td>{x.watchers}</td>
+            <td>{x.show ? x.show.year : x.year}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
 }
 
 Table.propTypes = {
-  headers: PropTypes.object.isRequired,
+  headers: PropTypes.instanceOf(Object).isRequired,
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
-}
+};
 
 export default Table;
