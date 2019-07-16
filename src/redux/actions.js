78d7dd317c13d1ab38e943cbb55885ca58b678c8
bpalij <bpalij@gmail.com>
2019-07-16 13:10:20 +0300

    Adapting actions.js to airbnb eslint

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,25 +1,19 @@
 import { START_LOAD_DATA, LOADED_DATA, CHANGED_PARAMS } from './actionTypes';
 
-export const startLoadData = () => {
-  return {
-    type: START_LOAD_DATA,
-  }
-}
+export const startLoadData = () => ({
+  type: START_LOAD_DATA,
+});
 
-export const loadedData = (data, headers, images) => {
-  return {
-    type: LOADED_DATA,
-    data: {
-      data,
-      headers,
-      images,
-    }
-  }
-}
+export const loadedData = (data, headers, images) => ({
+  type: LOADED_DATA,
+  data: {
+    data,
+    headers,
+    images,
+  },
+});
 
-export const changeParams = (params) => {
-  return {
-    type: CHANGED_PARAMS,
-    data: params,
-  }
-}
+export const changeParams = params => ({
+  type: CHANGED_PARAMS,
+  data: params,
+});
