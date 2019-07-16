1fe32787c8c8224f14b05a1c81e3c575f2158799
bpalij <bpalij@gmail.com>
2019-07-16 12:50:29 +0300

    Adapting Form to airbnb eslint
    Alerts mmust be fixed later

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,99 +4,109 @@ import headersToObj from '../misc/headersToObj';
 import getImageLinks from '../misc/getImageLinks';
 
 const yearOptions = [''];
-for(let i=((new Date()).getFullYear()); i>=1800; i--){
+for (let i = ((new Date()).getFullYear()); i >= 1800; i -= 1) {
   yearOptions.push(`${i}`);
-};
-const sortOptions = [
-  {value:'trending', text:'Trending'}, 
-  {value:'popular', text:'Popular'}, 
-  {value:'played/weekly', text:'Most played (weekly)'},
-  {value:'played/monthly', text:'Most played (monthly)'},
-  {value:'played/yearly', text:'Most played (yearly)'},
-  {value:'played/all', text:'Most played (all)'},
-  {value:'watched/weekly', text:'Most watched (weekly)'},
-  {value:'watched/monthly', text:'Most watched (monthly)'},
-  {value:'watched/yearly', text:'Most watched (yearly)'},
-  {value:'watched/all', text:'Most watched (all)'},
-  {value:'collected/weekly', text:'Most collected (weekly)'},
-  {value:'collected/monthly', text:'Most collected (monthly)'},
-  {value:'collected/yearly', text:'Most collected (yearly)'},
-  {value:'collected/all', text:'Most collected (all)'},
-  {value:'anticipated', text:'Most anticipated'},
-];
-
-function paramsQuery(filters){
-  if (filters.query && filters.year) {
-    return `${filters.sort}?query=${filters.query}&years=${filters.year}`;
-  } else if (filters.query) {
-    return `${filters.sort}?query=${filters.query}`;
-  } else if (filters.year) {
-    return `${filters.sort}?years=${filters.year}`;
-  } else {
-    return `${filters.sort}`;
-  }
 }
 
-class Form extends Component{
-  constructor(props){
+const sortOptions = [
+  { value: 'trending', text: 'Trending' },
+  { value: 'popular', text: 'Popular' },
+  { value: 'played/weekly', text: 'Most played (weekly)' },
+  { value: 'played/monthly', text: 'Most played (monthly)' },
+  { value: 'played/yearly', text: 'Most played (yearly)' },
+  { value: 'played/all', text: 'Most played (all)' },
+  { value: 'watched/weekly', text: 'Most watched (weekly)' },
+  { value: 'watched/monthly', text: 'Most watched (monthly)' },
+  { value: 'watched/yearly', text: 'Most watched (yearly)' },
+  { value: 'watched/all', text: 'Most watched (all)' },
+  { value: 'collected/weekly', text: 'Most collected (weekly)' },
+  { value: 'collected/monthly', text: 'Most collected (monthly)' },
+  { value: 'collected/yearly', text: 'Most collected (yearly)' },
+  { value: 'collected/all', text: 'Most collected (all)' },
+  { value: 'anticipated', text: 'Most anticipated' },
+];
+
+function paramsQuery(filters) {
+  if (filters.query && filters.year) {
+    return `${filters.sort}?query=${filters.query}&years=${filters.year}`;
+  }
+  if (filters.query) {
+    return `${filters.sort}?query=${filters.query}`;
+  }
+  if (filters.year) {
+    return `${filters.sort}?years=${filters.year}`;
+  }
+
+  return `${filters.sort}`;
+}
+
+class Form extends Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       query: '',
       year: '',
       sort: 'trending',
     };
-    this.buttonClick=this.buttonClick.bind(this);
+    this.buttonClick = this.buttonClick.bind(this);
   }
-  buttonClick(){
+
+  buttonClick() {
     const { changeParams, startLoadData, loadedData } = this.props;
-    const newFilters = {...this.state};
+    const newFilters = { ...this.state };
     changeParams(newFilters);
     startLoadData();
     let headers;
     let dataTemp;
-    fetch(`https://api.trakt.tv/shows/${paramsQuery(newFilters)}`,{
-      headers:{
+    fetch(`https://api.trakt.tv/shows/${paramsQuery(newFilters)}`, {
+      headers: {
         'Content-Type': 'application/json',
         'trakt-api-version': '2',
-        'trakt-api-key': `${process.env.REACT_APP_TRAKT_CLIENT_ID}`
-      }
+        'trakt-api-key': `${process.env.REACT_APP_TRAKT_CLIENT_ID}`,
+      },
     })
       .then((res) => {
         if (res.ok) {
           headers = headersToObj(res.headers);
           return res.json();
-        } else {
-          throw new Error(`Error ${res.status}: ${res.statusText}`);
         }
+        throw new Error(`Error ${res.status}: ${res.statusText}`);
       })
       .then((data) => {
-        dataTemp=data;
+        dataTemp = data;
         return getImageLinks(data);
       })
-      .then((img) => {loadedData(dataTemp, headers, img);})
-      .catch((e) => {alert(`Error '${e}', try to reload page`)});
+      .then((img) => { loadedData(dataTemp, headers, img); })
+      .catch((e) => { alert(`Error '${e}', try to reload page`); });
   }
-  render(){
+
+  render() {
     const { query, year, sort } = this.state;
     const { disableInput } = this.props;
     return (
       <form className="form-table-width main-form">
         <div className="form-flex-line">
           <div className="form-item">
-            <label htmlFor="query-input">Query</label>
-            <input type="text" id="query-input" value={query} disabled={disableInput} onChange={(e) => {this.setState({query: e.target.value})}}/>
+            <label htmlFor="query-input">
+              Query
+              <input type="text" id="query-input" value={query} disabled={disableInput} onChange={(e) => { this.setState({ query: e.target.value }); }} />
+            </label>
           </div>
           <div className="form-item">
-            <label htmlFor="year-select">Year</label>
-            <select id="year-select" value={year} disabled={disableInput} onChange={(e) => {this.setState({year: e.target.value})}}>
-              {yearOptions.map((x)=>(<option value={x} key={x}>{x}</option>))}
-            </select>
+            <label htmlFor="year-select">
+              Year
+              <select id="year-select" value={year} disabled={disableInput} onChange={(e) => { this.setState({ year: e.target.value }); }} onBlur={(e) => { this.setState({ year: e.target.value }); }}>
+                {yearOptions.map(x => (<option value={x} key={x}>{x}</option>))}
+              </select>
+            </label>
           </div>
           <div className="form-item">
-            <label htmlFor="sort-select">Sort</label>
-            <select id="sort-select" value={sort} disabled={disableInput} onChange={(e) => {this.setState({sort: e.target.value})}}>
-              {sortOptions.map((x) => (<option key={x.value} value={x.value}>{x.text}</option>))}
-            </select>
+            <label htmlFor="sort-select">
+              Sort
+              <select id="sort-select" value={sort} disabled={disableInput} onChange={(e) => { this.setState({ sort: e.target.value }); }} onBlur={(e) => { this.setState({ sort: e.target.value }); }}>
+                {sortOptions.map(x => (<option key={x.value} value={x.value}>{x.text}</option>))}
+              </select>
+            </label>
           </div>
         </div>
         <div className="form-flex-right-button">
@@ -105,7 +115,7 @@ class Form extends Component{
           </div>
         </div>
       </form>
-    )
+    );
   }
 }
 
@@ -114,6 +124,6 @@ Form.propTypes = {
   startLoadData: PropTypes.func.isRequired,
   loadedData: PropTypes.func.isRequired,
   changeParams: PropTypes.func.isRequired,
-}
+};
 
 export default Form;
